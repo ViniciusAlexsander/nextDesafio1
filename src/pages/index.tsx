3a9eb7e50e5f9041ab17158de0d4a8713289941c
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2022-01-10 22:13:04 -0300

    começando o desafio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import { AiOutlineCalendar } from 'react-icons/ai';
+import { MdPersonOutline } from 'react-icons/md';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +26,42 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.posts}>
+        <a className={styles.post}>
+          <strong>Como utilizar Hooks</strong>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.postInfo}>
+            <div>
+              <AiOutlineCalendar className={styles.postIcon} />
+              15 Mar 2021
+            </div>
+            <div>
+              <MdPersonOutline className={styles.postIcon} />
+              Joseph Oliveira
+            </div>
+          </div>
+        </a>
+        <a className={styles.post}>
+          <strong>Como utilizar Hooks</strong>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.postInfo}>
+            <div>
+              <AiOutlineCalendar className={styles.postIcon} />
+              15 Mar 2021
+            </div>
+            <div>
+              <MdPersonOutline className={styles.postIcon} />
+              Joseph Oliveira
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
